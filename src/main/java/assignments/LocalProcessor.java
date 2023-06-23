6e90caa3ee20a26d3fb7fb2785cd0e015b369c84
KaterynaKucherenko <estorskaya@gmail.com>
2023-06-23 23:17:14 +0300

    5th

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -1,6 +1,7 @@
 package assignments;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -21,7 +22,8 @@ public class LocalProcessor {
     private Integer valueOfCheap;
     Scanner informationScanner;
     static List<String> stringArrayList = new LinkedList<>();
-
+    public LocalProcessor() {
+    }
     public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
                           Scanner informationScanner, List<String> stringArrayList) {
         this.processorName = new StringBuilder(processorName);
@@ -31,40 +33,60 @@ public class LocalProcessor {
         this.informationScanner = informationScanner;
         LocalProcessor.stringArrayList = stringArrayList;
     }
-    
+
 
     @ListIteratorAnnotation
-    public void listIterator(LinkedList<String> stringList) {
-        if (stringList.size() !=0){
-        stringArrayList = new LinkedList<>(stringList);
-        stringArrayList.forEach(System.out::println);
-    }}
+    public void listIterator(List<String> stringList)
+    {
+        if (stringList.size() != 0){
+            stringArrayList = new LinkedList<>(stringList);
+            for (String element : stringList)
+            {
+                if (element != null){
+                    System.out.println(element.hashCode());
+                }
+            }
+        }
+    }
 
     @FullNameProcessorGeneratorAnnotation
-    public String fullNameProcessorGenerator(LinkedList<String> stringList) {
-            StringBuilder sb = new StringBuilder(processorName);
-            if(sb!=null){
-            for (String str : stringList) {
-                sb.append(str);
-            }}
-
-        return sb.toString();}
-
+    public String fullNameProcessorGenerator(List<String> stringList)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (processorName != null){
+            stringBuilder.append(processorName);
+        }
+        if (stringList.size() != 0){
+            for (String element : stringList)
+            {
+                if (element != null && !element.isEmpty()){
+                    stringBuilder
+                            .append(element)
+                            .append(" ");
+                }
+            }
+        }
+        return String.valueOf(stringBuilder);
+    }
 
     @ReadFullProcessorNameAnnotation
-    public void readFullProcessorName(File file) {
-        try{
-            informationScanner = new Scanner(file);
-            while (informationScanner.hasNext()) {
-                if (informationScanner!=null){
-                processorVersion.append(informationScanner.nextLine());
-            }}}
-        catch(Exception e){
+    public void readFullProcessorName(File file)
+    {
+        try (Scanner scanner = new Scanner(file))
+        {
+            informationScanner = scanner;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(processorVersion);
+            while (informationScanner.hasNext())
+            {
+                if (informationScanner.nextLine() != null){
+                    stringBuilder.append(informationScanner.nextLine());
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
             e.printStackTrace();
         }
-        finally {
-            informationScanner.close();
-        }
-
     }
 }
