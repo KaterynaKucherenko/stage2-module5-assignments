202688f42e82898af83fb97d9f5ea50834bd5487
KaterynaKucherenko <estorskaya@gmail.com>
2023-06-23 23:09:58 +0300

    delete const

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -31,9 +31,7 @@ public class LocalProcessor {
         this.informationScanner = informationScanner;
         LocalProcessor.stringArrayList = stringArrayList;
     }
-
-    public LocalProcessor() {
-    }
+    
 
     @ListIteratorAnnotation
     public void listIterator(LinkedList<String> stringList) {
